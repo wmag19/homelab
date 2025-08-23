53ff6d0e9f512da731c56be50f0597996e7fe91c
Will Maguire <will_maguire99@protonmail.com>
2025-08-23 13:08:15 +0100

    Fix providers

--- infra/bootstrap.tf
+++ infra/bootstrap.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-  }
-}
-
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
